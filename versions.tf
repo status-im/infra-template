d2fffa77b6cc5a9caf11c5a71304d7a8d43da23c
Jakub Sokołowski <jakub@status.im>
2022-05-11 14:29:16 +0200

    add Terraform boilerplate
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ versions.tf
@@ -0,0 +1,14 @@
+
+terraform {
+  required_version = "~> 1.1.0"
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = " = 2.21.0"
+    }
+    pass = {
+      source  = "camptocamp/pass"
+      version = " = 2.0.0"
+    }
+  }
+}
