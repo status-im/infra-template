09365842484b3cf1915dc2353ec4daa74d353e13
Jakub Sokołowski <jakub@status.im>
2024-06-20 11:41:21 +0200

    main.tf: fix path to clearly show placeholder
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
     /* KV store has a limit of 512KB */
     gzip = true
     /* WARNING This needs to be changed for every repo. */
-    path      = "terraform/codex/"
+    path      = "terraform/FLEET_NAME_PLACEHOLDER/"
     ca_file   = "ansible/files/consul-ca.crt"
     cert_file = "ansible/files/consul-client.crt"
     key_file  = "ansible/files/consul-client.key"
